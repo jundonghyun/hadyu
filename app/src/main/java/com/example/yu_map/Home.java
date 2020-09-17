046f0d508a989b29d95cc5480b66d9c204af8405
Git <jundonghyun>
2020-09-17 10:06:53 +0900

    Not login feature with firebase

---
+++ app/src/main/java/com/example/yu_map/Home.java
@@ -0,0 +1,41 @@
+package com.example.yu_map;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+//import com.example.yumap_tmap.R;
+
+public class Home extends AppCompatActivity {
+    /* access modifiers changed from: protected */
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView((int) R.layout.activity_home);
+        ((Button) findViewById(R.id.loginbutton)).setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            }
+        });
+        ((Button) findViewById(R.id.map)).setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Home.this.startActivity(new Intent(Home.this.getApplicationContext(), MainActivity.class));
+            }
+        });
+        ((Button) findViewById(R.id.finishbutton)).setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                new AlertDialog.Builder(Home.this).setTitle((CharSequence) "YuApp 종료").setMessage((CharSequence) "어플을 종료하시겠습니까?").setPositiveButton((CharSequence) "확인", (DialogInterface.OnClickListener) new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(Home.this, "YuApp이 종료되었습니다", Toast.LENGTH_LONG).show();
+                        Home.this.finish();
+                        ActivityCompat.finishAffinity(Home.this);
+                        System.exit(0);
+                    }
+                });
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/yu_map/MapPoint.java
@@ -0,0 +1,41 @@
+package com.example.yu_map;
+
+
+public class MapPoint {
+    private String Name;
+    private double latitude;
+    private double longitude;
+
+    public MapPoint() {
+    }
+
+    public MapPoint(String Name2, double latitude2, double longitude2) {
+        this.Name = Name2;
+        this.latitude = latitude2;
+        this.longitude = longitude2;
+    }
+
+    public String getName() {
+        return this.Name;
+    }
+
+    public void setName(String Name2) {
+        this.Name = Name2;
+    }
+
+    public double getLatitude() {
+        return this.latitude;
+    }
+
+    public void setLatitude(double latitude2) {
+        this.latitude = latitude2;
+    }
+
+    public double getLongitude() {
+        return this.longitude;
+    }
+
+    public void setLongitude(double longitude2) {
+        this.longitude = longitude2;
+    }
+}
