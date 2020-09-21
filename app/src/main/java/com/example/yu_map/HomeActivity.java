248464afb010e326f901ffcd9714284bff1637a7
Git <jundonghyun>
2020-09-21 15:12:02 +0900

    사용자 로그인 여부 확인 기능 추가 & recycler view 사용해서 친구 목록 생성

--- app/src/main/java/com/example/yu_map/HomeActivity.java
+++ app/src/main/java/com/example/yu_map/HomeActivity.java
@@ -1,28 +1,54 @@
 package com.example.yu_map;
 
+import android.Manifest;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.GeoPoint;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.HashMap;
+import java.util.Map;
 //import com.example.yumap_tmap.R;
 
 public class HomeActivity extends AppCompatActivity {
 
     private static final String TAG = "HomeActivitiy";
-    private Button shutdown, Login, Map, FriendLocation;
+    private Button Login, Map, FriendLocation;
+    private final int MY_PERMISSION_REQUEST_LOCATION = 1001;
+    private FusedLocationProviderClient fusedLocationClient;
+    private String Email = ((LoginActivity) LoginActivity.context).GlobalEmail;
+
 
     /* access modifiers changed from: protected */
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView((int) R.layout.activity_home);
 
-        shutdown = findViewById(R.id.finishbutton);
         Login = findViewById(R.id.loginbutton);
         Map = findViewById(R.id.map);
         FriendLocation = findViewById(R.id.FriendLocationButton);
@@ -45,9 +71,23 @@ public class HomeActivity extends AppCompatActivity {
                 HomeActivity.this.startActivity(new Intent(HomeActivity.this.getApplicationContext(), MainActivity.class));
             }
         });
-        shutdown.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
 
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item){
+        Toast toast = Toast.makeText(getApplicationContext(), "", Toast.LENGTH_LONG );
+
+        switch (item.getItemId()){
+            case R.id.Logout:
                 Toast.makeText(HomeActivity.this, "YuApp이 종료되었습니다", Toast.LENGTH_LONG).show();
 
                 /* 어플 종료 방법 */
@@ -56,7 +96,126 @@ public class HomeActivity extends AppCompatActivity {
                 finishAndRemoveTask(); // 2단계 액티비티종료 + 태스크 리스트에서 지우기
                 android.os.Process.killProcess(android.os.Process.myPid()); // 3단계 앱 프로세스 종료
                 Log.d(TAG, "application shutdown");
-            }
-        });
+
+                break;
+
+            case R.id.Location:
+
+
+                final java.util.Map<String, Double> User = new HashMap<>();
+                final FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+
+                /*위치 권환 확인*/
+                int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
+                if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+                    if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                            Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+                    } else {
+                        ActivityCompat.requestPermissions(this,
+                                new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                                MY_PERMISSION_REQUEST_LOCATION);
+                    }
+                }
+
+                fusedLocationClient.getLastLocation()
+                        .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                            @Override
+                            public void onSuccess(android.location.Location location) {
+                                if (location != null) {
+                                    final GeoPoint geoPoint = new GeoPoint(
+                                            location.getLatitude(), location.getLongitude()
+                                    );
+
+                                    db.collection("UserLocation")
+                                            .get()
+                                            .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                                                @Override
+                                                public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                                    if(task.isSuccessful()){
+
+                                                        /* 새로운 UserLocation생성 */
+                                                        if(task.getResult().size() > 0){
+                                                            User.put("Latitude", geoPoint.getLatitude());
+                                                            User.put("Longitude", geoPoint.getLongitude());
+
+                                                            DocumentReference newUserRef = db
+                                                                    .collection("UserLocation")
+                                                                    .document(Email);
+
+                                                            newUserRef.set(User)
+                                                                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                                        @Override
+                                                                        public void onComplete(@NonNull Task<Void> task) {
+                                                                            Log.d(TAG, "GeoPoint successfully Written");
+
+                                                                        }
+                                                                    })
+                                                                    .addOnFailureListener(new OnFailureListener() {
+                                                                        @Override
+                                                                        public void onFailure(@NonNull Exception e) {
+                                                                            Log.d(TAG, "GeoPoint Writing Failure");
+
+                                                                        }
+                                                                    });
+                                                        }
+                                                    }
+                                                    /* email로 시작하는 UserLocation이 있는경우 */
+
+                                                    else{
+                                                        DocumentReference newUserLocationRef = db
+                                                                .collection("UserLocation")
+                                                                .document(Email);
+
+                                                        newUserLocationRef
+                                                                .update("Longitude", geoPoint.getLongitude())
+                                                                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                                                    @Override
+                                                                    public void onSuccess(Void aVoid) {
+                                                                        Log.d(TAG, "DocumentSnapshot added");
+
+                                                                    }
+                                                                })
+                                                                .addOnFailureListener(new OnFailureListener() {
+                                                                    @Override
+                                                                    public void onFailure(@NonNull Exception e) {
+                                                                        Log.d(TAG, "Error adding document", e);
+
+                                                                    }
+                                                                });
+
+                                                        newUserLocationRef
+                                                                .update("Latitude", geoPoint.getLatitude())
+                                                                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                                                    @Override
+                                                                    public void onSuccess(Void aVoid) {
+                                                                        Log.d(TAG, "DocumentSnapshot added");
+
+                                                                    }
+                                                                })
+                                                                .addOnFailureListener(new OnFailureListener() {
+                                                                    @Override
+                                                                    public void onFailure(@NonNull Exception e) {
+                                                                        Log.d(TAG, "Error adding document", e);
+                                                                    }
+                                                                });
+                                                    }
+                                                }
+                                            });
+                                }
+                            }
+                        });
+                break;
+
+            case R.id.Friend:
+                startActivity(new Intent(HomeActivity.this,AddFriendActivity.class));
+
+                break;
+        }
+        toast.show();
+
+        return super.onOptionsItemSelected(item);
     }
+
 }
---
+++ app/src/main/java/com/example/yu_map/RecyclerAdapter.java
@@ -0,0 +1,65 @@
+package com.example.yu_map;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ItemViewHolder> {
+
+    private ArrayList<Data> listData = new ArrayList<>();
+
+    @NonNull
+    @Override
+    public RecyclerAdapter.ItemViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item, parent, false);
+
+
+        return new ItemViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerAdapter.ItemViewHolder holder, int position) {
+        holder.onBind(listData.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return listData.size();
+    }
+
+    void addItem(Data data){
+        listData.add(data);
+    }
+
+    class ItemViewHolder extends RecyclerView.ViewHolder{
+
+        private TextView textView1;
+        private TextView textView2;
+        private ImageView imageView;
+
+
+        ItemViewHolder(View itemView){
+            super(itemView);
+
+            textView1 = itemView.findViewById(R.id.textView1);
+            textView2 = itemView.findViewById(R.id.textView2);
+            imageView = itemView.findViewById(R.id.imageView);
+        }
+
+        void onBind(Data data){
+            textView1.setText(data.getTitle());
+            textView2.setText(data.getContent());
+            imageView.setImageResource(data.getResId());
+
+        }
+
+    }
+}
