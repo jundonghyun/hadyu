338450050ff1b90e6d4ffa97d0112aec638911b8
Git <jundonghyun>
2020-09-24 15:00:55 +0900

    친구추가시 사용자 확인 성공까지 완료 확인실패 구현해야함

--- app/src/main/java/com/example/yu_map/Activity/AddFriendPopUpActivity.java
+++ app/src/main/java/com/example/yu_map/Activity/AddFriendPopUpActivity.java
@@ -1,15 +1,38 @@
 package com.example.yu_map.Activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
+import com.example.yu_map.AddFriendActivity;
 import com.example.yu_map.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.SignInMethodQueryResult;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
 
 public class AddFriendPopUpActivity extends AppCompatActivity {
 
+    private final String TAG = "AddFriendPopUpActivity";
     EditText id;
+    Button btn;
+    String idTemp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +40,44 @@ public class AddFriendPopUpActivity extends AppCompatActivity {
         setContentView(R.layout.activity_add_friend_pop_up);
 
         id = (EditText)findViewById(R.id.friend_id);
+        btn = (Button)findViewById(R.id.OK);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AddFriend();
+
+
+                startActivity(new Intent(AddFriendPopUpActivity.this,AddFriendActivity.class));
+            }
+        });
     }
+
+    private void AddFriend() {
+
+        idTemp = id.getText().toString();
+        final FirebaseFirestore db = FirebaseFirestore.getInstance();
+        CollectionReference col = db.collection("User");
+        Query query = col.whereEqualTo("Email", idTemp);
+
+        db.collection("User")
+                .whereEqualTo("Email", idTemp)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Toast.makeText(AddFriendPopUpActivity.this, "친구추가 완료", Toast.LENGTH_LONG).show();
+
+                            }
+                        }
+                        else{
+                            Toast.makeText(AddFriendPopUpActivity.this, "사용자를 찾을 수 없습니다", Toast.LENGTH_LONG).show();
+                            Log.d(TAG, "Error");
+                        }
+                    }
+                });
+    }
+
 }
--- app/src/main/java/com/example/yu_map/AddFriendActivity.java
+++ app/src/main/java/com/example/yu_map/AddFriendActivity.java
@@ -1,21 +1,37 @@
 package com.example.yu_map;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 
 import com.example.yu_map.Activity.AddFriendPopUpActivity;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.SignInMethodQueryResult;
 
 public class AddFriendActivity extends AppCompatActivity {
+
+    private final String TAG = "AddFriendActivity";
+
+
+    String FriendEmail = "123";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_friend);
+
+
     }
 
     public boolean onCreateOptionsMenu(Menu menu){
@@ -30,10 +46,13 @@ public class AddFriendActivity extends AppCompatActivity {
         switch (item.getItemId()){
             case R.id.AddFriend:
                 startActivity(new Intent(AddFriendActivity.this, AddFriendPopUpActivity.class));
+
                 break;
 
         }
 
         return super.onOptionsItemSelected(item);
     }
+
+
 }
